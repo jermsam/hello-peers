174e097ebb36f697d8048953d05e06d994ec507d
jermsam <ssali@jitpomi.com>
2023-05-27 07:56:13 -0600

    vue client

--- client-vue/tailwind.config.js
+++ client-vue/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  content: [],
+  content: [
+    './index.html', './src/**/*.{vue,js,ts,jsx,tsx}'
+  ],
   theme: {
     extend: {},
   },
