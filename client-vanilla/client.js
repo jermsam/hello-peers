b93209f2390531ec6a155819bf75ddf5c8195480
Ruulul <vamjunior01@gmail.com>
2023-05-28 11:45:40 -0300

    syncronize view with db updates from anywhere

--- client-vanilla/client.js
+++ client-vanilla/client.js
@@ -6,8 +6,8 @@ import goodbye from 'graceful-goodbye'
 import b4a from 'b4a'
 
 import * as SDK from 'hyper-sdk'
-import { createMultiWriterDB } from './db'
-import { setTodo, todoList } from './view'
+import { createMultiWriterDB, createDB } from './db'
+import { setTodo, createTodo, configTodo, todoList } from './view'
 const { crypto, WebSocket } = window
 
 let resolveReady
@@ -30,10 +30,11 @@ const topicBuffer = await crypto.subtle.digest('SHA-256', b4a.from('say a good h
 const discovery = await sdk.get(topicBuffer)
 
 discovery.on('peer-add', peerInfo => {
-  console.log('new peer, peers count:', discovery.peers.length)
+  console.log('new peer, peer:', peerInfo, 'peer count:', discovery.peers.length)
 })
 
 const db = await createMultiWriterDB(sdk, discovery)
+// const db = createDB(await sdk.getBee('todo-app'))
 goodbye(async () => {
   await db.close()
   await discovery.close()
@@ -42,11 +43,41 @@ goodbye(async () => {
 const todoCollection = db.collection('todo')
 await todoCollection.createIndex(['text'])
 await todoCollection.createIndex(['done', 'text'])
+// db.bee.core.on('append', async () => {
+console.log(db.autobase)
+db.autobase.on('append', async () => {
+  console.log(db.autobase)
+  for await (const todo of todoCollection.find()) {
+    console.log('a doc')
+    const todoElement = document.getElementById(todo._id.toString())
+    if (!todoElement) {
+      setTodo(todo)
+    } else {
+      const toReplaceWith = createTodo(todo)
+      if (todoElement.innerHTML === toReplaceWith.innerHTML) continue
+      console.log('different todo detected')
+      todoElement.replaceWith(toReplaceWith)
+      configTodo(todo)
+    }
+  }
+  todoList.querySelectorAll('section').forEach(async section => {
+    if ((await todoCollection.find({ _id: section.id })).length === 0) section.remove()
+  })
+})
 
 resolveReady()
 
 sdk.joinCore(discovery).then(() => console.log('discovering'))
 
 export function addTodo (todo) {
-  ready.then(() => todoCollection.insert(todo)).then(setTodo)
+  ready.then(() => todoCollection.insert(todo))
+}
+export function toggleTodo (_id) {
+  return todoCollection.find({ _id }).then(([todo]) => {
+    if (todo.done) todoCollection.update({ _id }, { done: false })
+    else todoCollection.update({ _id }, { done: true })
+  })
+}
+export function deleteTodo (_id) {
+  return todoCollection.delete({ _id })
 }
--- client-vanilla/view.js
+++ client-vanilla/view.js
@@ -1,4 +1,4 @@
-import { addTodo } from './client'
+import { addTodo, toggleTodo, deleteTodo } from './client'
 
 const todoDialogOpenButton = document.getElementById('todo-dialog-open-button')
 const todoDialogCloseButton = document.getElementById('todo-dialog-close-button')
@@ -8,6 +8,23 @@ export const todoList = document.getElementById('todo-list')
 const todoText = document.getElementById('todo-text')
 
 export function setTodo (todo) {
+  todoList.appendChild(createTodo(todo))
+  configTodo(todo)
+}
+export function configTodo (todo) {
+  document.getElementById(`checkbox-${todo._id}`).innerHTML = todo.done
+    ? '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">\n' +
+    '  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />\n' +
+    '</svg>'
+    : '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">\n' +
+    '  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />\n' +
+    '</svg>'
+  document.getElementById(`checkbox-${todo._id}`).onclick = () => toggleTodo(todo._id)
+  document.getElementById(`delete-${todo._id}`).onclick = () => deleteTodo(todo._id)
+  const textLine = document.getElementById(`text-${todo._id}`)
+  todo.done ? textLine.classList.add('line-through') : textLine.classList.remove('line-through')
+}
+export function createTodo (todo) {
   const section = document.createElement('section')
   section.id = todo._id.toString()
   section.innerHTML = `
@@ -21,22 +38,13 @@ export function setTodo (todo) {
                                 <h2 id="text-${todo._id}" class="text-lg font-medium text-gray-700 sm:text-2xl dark:text-gray-950"> ${todo.text}</h2>
                             </div>
                         </div>
-                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 cursor-pointer">
+                         <svg id="delete-${todo._id}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 cursor-pointer">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                   </svg>
                     </div>
 `
-  todoList.appendChild(section)
 
-  document.getElementById(`checkbox-${todo._id}`).innerHTML = todo.done
-    ? '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">\n' +
-    '  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />\n' +
-    '</svg>'
-    : '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">\n' +
-    '  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />\n' +
-    '</svg>'
-  const textLine = document.getElementById(`text-${todo._id}`)
-  todo.done ? textLine.classList.add('line-through') : textLine.classList.remove('line-through')
+  return section
 }
 
 todoForm.addEventListener('submit', (e) => {
