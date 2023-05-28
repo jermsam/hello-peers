0318468ad8dba65f47da09474f9b06055a28eaec
Ruulul <vamjunior01@gmail.com>
2023-05-28 04:29:05 -0300

    using hyper-sdk and extention core messages

--- client-vanilla/client.js
+++ client-vanilla/client.js
@@ -1,79 +1,77 @@
 import DHT from '@hyperswarm/dht-relay'
-import Stream from '@hyperswarm/dht-relay/ws';
+import Stream from '@hyperswarm/dht-relay/ws'
 // @ts-ignore
-import Hyperswarm from 'hyperswarm';
-// @ts-ignore
-import goodbye from 'graceful-goodbye';
+import goodbye from 'graceful-goodbye'
 // import * as BufferSource from 'buffer/'
 import b4a from 'b4a'
+import * as SDK from 'hyper-sdk'
 
 const socket = new WebSocket('ws://localhost:3400')
 const dht = new DHT(new Stream(true, socket))
-console.log(dht);
 
-
-const swarm = new Hyperswarm({dht});
+const sdk = await SDK.create({
+  storage: 'demo-p2p-todo-app',
+  swarmOpts: {
+    dht
+  }
+})
 
 const topicBuffer = b4a.from('say a good hello', 'hex')
 
-swarm.join(topicBuffer)
+const discovery = await sdk.get(topicBuffer)
 
 goodbye(async () => {
-  await swarm.leave(topicBuffer)
-  await swarm.connections.forEach((conn) => conn.close())
-  await swarm.destroy()
+  await discovery.close()
+  await sdk.close()
 })
 
-
-swarm.on('connection', (conn, peerInfo) => {
-  console.log('new peer connected', peerInfo)
-  conn.on('data', (dataUpdate) => {
-    console.log('updated data: ', b4a.toString(dataUpdate));
-    const jsonData = b4a.toString(dataUpdate);
-    setTodo(JSON.parse(jsonData))
-  })
-});
+discovery.on('new-peer', peerInfo => {
+  console.log('new peer:', peerInfo.publicKey.toString('hex'))
+})
+const newDataExt = discovery.registerExtension(topicBuffer + 'new-data', {
+  format: 'json',
+  onmessage: setTodo
+})
 
 const todoDialogOpenButton = document.getElementById('todo-dialog-open-button')
 const todoDialogCloseButton = document.getElementById('todo-dialog-close-button')
 const todoDialog = document.getElementById('todo-dialog')
 const todoForm = document.getElementById('todo-form')
 const todoList = document.getElementById('todo-list')
-const todoText = document.getElementById("todo-text");
-function setTodo(todo) {
+const todoText = document.getElementById('todo-text')
+function setTodo (todo) {
   const li = document.createElement('li')
-  console.log(todo);
-  li.innerHTML = todo.text;
+  console.log(todo)
+  li.innerHTML = todo.text
   todoList.appendChild(li)
 }
 
-todoForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  
-  if (todoText.value === "") {
+todoForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  if (todoText.value === '') {
     // throw error
-    const err = 'please enter a todo text';
-    console.error(err);
-    document.getElementById("todo-error").innerHTML = err;
+    const err = 'please enter a todo text'
+    console.error(err)
+    document.getElementById('todo-error').innerHTML = err
   } else {
     // perform operation with form input
     const todo = {
       text: todoText.value,
       done: false
     }
-    swarm.connections.forEach((conn) =>  conn.write(JSON.stringify(todo)))
+    newDataExt.broadcast(todo)
     setTodo(todo)
-    todoText.value=''
+    todoText.value = ''
     todoDialog.close()
   }
   // handle submit
-});
+})
 
-todoDialogOpenButton.addEventListener('click', function openDialog() {
+todoDialogOpenButton.addEventListener('click', function openDialog () {
   todoDialog.showModal()
 })
 
-todoDialogCloseButton.addEventListener('click', function closeDialog() {
+todoDialogCloseButton.addEventListener('click', function closeDialog () {
   todoDialog.close()
 })
-
