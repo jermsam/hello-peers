3d78beccd42056d78d01c6868cd6e8139b51c519
Ruulul <vamjunior01@gmail.com>
2023-05-28 23:52:04 -0300

    setup for better update logic and history stream

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -6,6 +6,8 @@ import goodbye from 'graceful-goodbye'
 import b4a from 'b4a'
 
 import * as SDK from 'hyper-sdk'
+import { RangeWatcher } from '@lejeunerenard/hyperbee-range-watcher-autobase'
+import { BSON } from 'bson'
 import { createMultiWriterDB } from './db.js'
 import { setTodo, createTodo, configTodo, todoList } from './view.js'
 const { crypto, WebSocket } = window
@@ -34,8 +36,6 @@ discovery.on('peer-add', peerInfo => {
 })
 
 const db = await createMultiWriterDB(sdk, discovery)
-const streamBee = bee => bee.createHistoryStream({ live: true })
-db.autobase.ready().then(_ => consumeHistoryStream(streamBee(db.bee)))
 goodbye(async () => {
   await db.close()
   await discovery.close()
@@ -44,21 +44,24 @@ goodbye(async () => {
 const todoCollection = db.collection('todo')
 await todoCollection.createIndex(['text'])
 await todoCollection.createIndex(['done', 'text'])
+// ready.then(createWatcher)
+
 db.autobase.on('append', async () => {
   for await (const todo of todoCollection.find()) {
     const todoElement = document.getElementById(todo._id.toString())
-    if (!todoElement) {
-      setTodo(todo)
-    } else {
+    if (!todoElement) setTodo(todo)
+    else {
       const toReplaceWith = createTodo(todo)
       if (todoElement.innerHTML === toReplaceWith.innerHTML) continue
       todoElement.replaceWith(toReplaceWith)
       configTodo(todo)
     }
   }
-  todoList.querySelectorAll('section').forEach(async section => {
-    if ((await todoCollection.find({ _id: section.id })).length === 0) section.remove()
-  })
+  todoList.querySelectorAll('section').forEach(todo =>
+    todoCollection.find({ _id: todo.id }).then(query => {
+      if (query.length === 0) todo.remove()
+    })
+  )
 })
 
 resolveReady()
@@ -78,9 +81,28 @@ export function deleteTodo (_id) {
   return todoCollection.delete({ _id })
 }
 
-async function consumeHistoryStream (stream) {
-  for await (const entry of stream) {
-    console.log('history entry:')
-    console.log(entry)
+function createWatcher () {
+  return new RangeWatcher(db.autobase.view, {}, null, updateStream)
+}
+async function updateStream (node) {
+  const { key, value, type } = node
+  console.log('watched saw', {
+    key: String.fromCharCode(...key).split('\u0000').join('/'),
+    value: String.fromCharCode(...key).split('\u0000').join('/'),
+    type
+  })
+  if (b4a.includes(key, 'doc')) {
+    const doc = BSON.deserialize(value)
+    const todoElement = document.getElementById(doc._id.toString())
+    if (type === 'put') {
+      if (!todoElement) return setTodo(doc)
+      const toReplaceWith = createTodo(doc)
+      if (todoElement.innerHTML === toReplaceWith.innerHTML) return
+      todoElement.replaceWith(toReplaceWith)
+      configTodo(doc)
+    }
+    if (type === 'del') {
+      todoElement.remove()
+    }
   }
 }
